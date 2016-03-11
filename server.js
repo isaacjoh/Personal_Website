430badaa95b6840687be208d40259aea234b1247
Isaac Oh <isaacohishere@gmail.com>
2016-03-11 13:39:47 -0800

    Changed view name to index.html

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req,res) {
-	res.sendFile(__dirname + '/public/views/SwanCleaners1.html', {
+	res.sendFile(__dirname + '/public/views/index.html', {
 		title: 'Swan Cleaners'
 	});
 });
